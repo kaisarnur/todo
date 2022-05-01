1979e47eec426a051b7fb7389965d40dab8176a4
kaisarnur <itmkaisar@gmail.com>
2022-05-01 16:55:31 +0600

    Создал модельку таска

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    title = models.CharField(max_length=100, null=False, blank=False, verbose_name="Title")
+    description = models.TextField(max_length=3000, null=False, blank=False, verbose_name="Description")
+    execution_time = models.DateTimeField(null=False, blank=False, verbose_name="Execution Time")
+    is_done = models.BooleanField(default=False, verbose_name="Is done")
+
+    def __str__(self):
+        return self.title
